1b553a21faadececb0a857da4f47b20eebfdfe38
patjawat <patjawat@gmail.com>
2023-03-10 10:33:42 +0700

    u

--- bs-frontend/src/app/modules/post/form-post/form-post.component.ts
+++ bs-frontend/src/app/modules/post/form-post/form-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PostService } from 'src/app/core/services/post.service';
 
 @Component({
   selector: 'app-form-post',
@@ -7,9 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormPostComponent implements OnInit {
 
-  constructor() { }
+  postForm: FormGroup;
 
+  constructor(
+    private _fb: FormBuilder,
+    private postService: PostService
+  ) {
+ this.postForm = _fb.group({
+    title: '',
+    content: '',
+    category: '',
+  })
+  }
+    
+    
+    
+    
   ngOnInit(): void {
   }
+
+  onFormSubmit() {
+    // console.log(this.postForm.value)
+    // if (this.empForm.valid) {
+    //   if (this.data) {
+    //     this.employeeService.updateEmp(this.data.id,this.empForm.value).subscribe({
+    //       next: (val: any) => {
+    //         this._dialogRef.close(true);
+    //       },
+    //       error: (err: any) => {
+    //         console.error(err);
+    //       },
+
+    //     });
+    //   } else {
+
+        this.postService.create(this.postForm.value).subscribe({
+          next: (val: any) => {
+            console.log('success')
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+
+        });
+    //   }
+    // }
+
+
+  }
+ 
 
 }
--- bs-frontend/src/app/modules/post/post.component.ts
+++ bs-frontend/src/app/modules/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/core/auth/service/auth.service';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string = '';
+
+  constructor(
+    private modalService: NgbModal,
+    private authservice:AuthService
+    ) {}
 
   ngOnInit(): void {
+    console.log(this.authservice.GetToken())
+
+  }
+  open(content:any) {
+    this.modalService.open(content, {size:'lg',ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  } 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
