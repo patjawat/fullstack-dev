abda8357f9aeb434f337a0d869b4d577907159cc
patjawat <patjawat@gmail.com>
2023-03-20 16:04:41 +0700

    uu

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'http://127.0.0.1:3000/api/',
-    link:'http://localhost',
-    name:'development environment',
-};
+    production: false,
+    API: 'http://localhost:3000'
+  };
