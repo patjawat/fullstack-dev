57b822f07e032fcec778c2e44e61c1d4009305d3
patjawat <patjawat@gmail.com>
2023-06-20 23:28:11 +0700

    update

--- frontend/src/app/core/auth/guards/auth.guard.ts
+++ frontend/src/app/core/auth/guards/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private service:AuthService,private route:Router){
 
